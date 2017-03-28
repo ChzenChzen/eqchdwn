a8c664d062902efdd5640f1bda5489f56abf4ebd
ChzenChzen <fedorovdanila@gmail.com>
2017-03-29 00:56:26 +0300

    made comments and main functional was implemented

--- eq_checkdown.py
+++ eq_checkdown.py
@@ -5,6 +5,8 @@ file_name = "input.xlsx"
 
 
 def convert_xlsx_to_dataframe(file):
+    """grabs data from excel file and return dataframe with two columns (hands, hands_realize)"""
+
     hands_realize_list = []
     hands_list = []
     data = {}
@@ -25,24 +27,26 @@ def convert_xlsx_to_dataframe(file):
 
 
 def realize_report(x, y, dataframe):
+    """makes report in format avg realize and range"""
+
     hands_x_y = dataframe[(dataframe.hands_realize > x) & (dataframe.hands_realize < y)]
-    avg_realize = sum(hands_x_y.hands_realize) / len(hands_x_y.hands_realize)
-    cards = ""
-    for card in hands_x_y.hands:
-        cards = cards + card + ","
-    report = "avg: " + str(avg_realize) + " range: " + cards
-    return report
+    if len(hands_x_y.hands_realize) != 0:
+        avg_realize = sum(hands_x_y.hands_realize) / len(hands_x_y.hands_realize)
+        cards = ""
+        for card in hands_x_y.hands:
+            cards = cards + card + ","
+        report = "avg: " + str(avg_realize) + " range: " + cards
+        return report
+
+
+def output(dataframe, pace):
+    """Iterate through all data and save final report to txt file"""
+
+    output_file_name = "output.txt"
+    steps = range(0, max(dataframe.hands_realize), pace)
+    with open(output_file_name, 'w') as outfile:
+        for step in steps:
+            outfile.write(str(realize_report(step, step + pace, dataframe)) + "\n")
 
 df = convert_xlsx_to_dataframe(file_name)
-
-print(df)
-steps = range(0, len(df.hands) - 1)
-print(steps)
-
-for step in steps:
-    print()
-
-# print(realize_report(0, 10, df))
-
-# with open("output.txt", 'w') as f_obj:
-#     f_obj.write(cards_ranges)
+output(df, 10)
