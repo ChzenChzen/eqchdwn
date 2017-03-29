8b0f9ac913e3ab0c6eda7c4ff03957b574ea88f8
DannyZavr-PC <fedorovdanila@gmail.com>
2017-03-29 17:57:17 +0300

    Found critical logic bug and fix it. Source code was Refactored and improved

--- eq_checkdown.py
+++ eq_checkdown.py
@@ -10,20 +10,26 @@ def error_message():
 
 def slice_data_to_report(from_eq, to_eq, dataframe_name):
     """makes report in format avg realize and range"""
-    sliced_dataframe = dataframe_name[from_eq:to_eq]  # make new dataframe with values of Equity Realization
-    if not sliced_dataframe.empty:
-        avg_eq_realization = sum(sliced_dataframe[" Equity Realization"]) / len(
-            sliced_dataframe[" Equity Realization"])  # calculate average value of Equity Realization in this slice
+
+    sorted_dataframe = dataframe_name.sort_values(by=["EqR"],
+                                                  ascending=True)  # Sorts dataframe by equity
+    # make new dataframe with values between 'from_eq' and 'to_eq' variables.
+    sorted_sliced_dataframe = sorted_dataframe[
+        (sorted_dataframe["EqR"] > from_eq) & (sorted_dataframe["EqR"] < to_eq)]
+    if not sorted_sliced_dataframe.empty:
+        # calculate average value of Equity Realization in this slice
+        avg_eq_realization = sorted_sliced_dataframe["EqR"].mean()
         cards_range = ""
-        for card in sliced_dataframe["Hand"]:  # do range of cards from slice
-            cards_range = cards_range + card + ","
+        for card in sorted_sliced_dataframe["Hand"]:  # do range of cards from slice
+            cards_range += card + ","
+        # print report
         print("Values between {0!s} and {1!s}. Avg eq: {2:.2f} Range: {3!s}".format(from_eq, to_eq, avg_eq_realization,
                                                                                     cards_range[:-1]))
 
 
 def output(dataframe_name, pace):
     """Iterates through all data and save final report to txt file"""
-    biggest_eq_realization = int(max(dataframe_name[" Equity Realization"]))
+    biggest_eq_realization = int(max(dataframe_name["EqR"]))
     steps = range(0, biggest_eq_realization, pace)
     for step in steps:
         slice_data_to_report(step, step + pace, dataframe_name)
@@ -38,6 +44,7 @@ def loop():
             break
 
         df = pd.read_csv(file_name, usecols=["Hand", " Equity Realization"])
+        df.rename(columns={" Equity Realization": "EqR"}, inplace=True)
         output(df, pace)
 
 
